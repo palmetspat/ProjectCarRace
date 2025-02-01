d96438733cbb06700de5bddc317145184e3c5608
Patrick Palmetshofer <patrick.palmetshofer@icloud.com>
2025-02-01 21:51:04 +0100

    create classes

---
+++ CarRace.UnitTest/CarTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRace.UnitTest
+{
+    /// <summary>
+    /// Summary description for CarTests
+    /// </summary>
+    [TestClass]
+    public class CarTests
+    {
+  
+    }
+}
