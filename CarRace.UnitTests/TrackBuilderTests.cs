3281339887e5dc7810f327cca2bd2397ba1616e6
Patrick Palmetshofer <patrick.palmetshofer@icloud.com>
2025-02-01 23:01:57 +0100

    Add some Assert Tests in ItShouldBuildAConnectedTrack_GivenSectionInformation

--- CarRace.UnitTests/TrackBuilderTests.cs
+++ CarRace.UnitTests/TrackBuilderTests.cs
@@ -19,5 +19,21 @@ public class TrackBuilderTests
         secondSection.AddAfterMe(thirdSection);
         Track manuallyBuiltTrack = new([startSection, secondSection, thirdSection]);
 
+        Assert.AreEqual(10, manuallyBuiltTrack.StartSection!.Length);
+        Assert.AreEqual(10, manuallyBuiltTrack.StartSection.MaxSpeed);
+        Assert.AreEqual(startSection, manuallyBuiltTrack.StartSection);
+        Assert.AreEqual(secondSection, manuallyBuiltTrack.StartSection.NextSection);
+
+        Assert.AreEqual(10, builder.RaceTrack!.StartSection!.Length);
+        Assert.AreEqual(10, builder.RaceTrack.StartSection.MaxSpeed);
+
+        Assert.AreEqual(manuallyBuiltTrack.StartSection.Length, builder.RaceTrack.StartSection.Length);
+        Assert.AreEqual(manuallyBuiltTrack.StartSection.MaxSpeed, builder.RaceTrack.StartSection.MaxSpeed);
+        Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection!.Length, builder.RaceTrack.StartSection.NextSection!.Length);
+        Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection.MaxSpeed, builder.RaceTrack.StartSection.NextSection.MaxSpeed);
+        Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection.NextSection!.Length, builder.RaceTrack.StartSection.NextSection.NextSection!.Length);
+        Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection.NextSection.MaxSpeed, builder.RaceTrack.StartSection.NextSection.NextSection.MaxSpeed);
+
     }
+
 }
