9138bbe82b8784a66bd9d8372e85f39674cc6943
Patrick Palmetshofer <patrick.palmetshofer@icloud.com>
2025-02-01 22:58:11 +0100

    Add some Code in Track Class

--- CarRace.Logic/TrackBuilder.cs
+++ CarRace.Logic/TrackBuilder.cs
@@ -7,5 +7,35 @@ using System.Threading.Tasks;
 
 namespace CarRace.Logic
 {
-        
+    public class TrackBuilder
+    {
+        private (int, int)[] _sectionInfos;
+        private Track? _track;
+
+        public Track? RaceTrack => _track;
+
+        public TrackBuilder((int, int)[] sectionInfos, bool trackShallLoop = false)
+        {
+            _sectionInfos = sectionInfos;
+
+
+            List<Section> allSections = [];
+            Section? lastSection = null;
+
+            foreach (var section in _sectionInfos)
+            {
+                Section newSection = new(section.Item1, section.Item2);
+
+                if (allSections.Count > 0)
+                    lastSection!.AddAfterMe(newSection);
+
+                lastSection = newSection;
+                allSections.Add(newSection);
+            }
+            _track = new Track(allSections, trackShallLoop);
+
+        }
+
+
+    }
 }
--- CarRace.UnitTests/TrackBuilderTests.cs
+++ CarRace.UnitTests/TrackBuilderTests.cs
@@ -8,6 +8,18 @@ public class TrackBuilderTests
     [TestMethod]
     public void ItShouldBuildAConnectedTrack_GivenSectionInformation()
     {
+        (int, int)[] sectionInfos = [(10, 10), (20, 20), (30, 30)];
+
+        TrackBuilder builder = new(sectionInfos);
+
+        Section
+          startSection = new(sectionInfos[0].Item1, sectionInfos[0].Item2),
+          secondSection = new(sectionInfos[1].Item1, sectionInfos[1].Item2),
+          thirdSection = new(sectionInfos[2].Item1, sectionInfos[2].Item2);
+
+        startSection.AddAfterMe(secondSection);
+        secondSection.AddAfterMe(thirdSection);
+        Track manuallyBuiltTrack = new([startSection, secondSection, thirdSection]);
 
     }
 }
